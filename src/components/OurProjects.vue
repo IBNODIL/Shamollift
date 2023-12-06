5c04f2fa089d1eadb551a0231ff8d5d8ab5d2c65
IBNODIL <ibnodil2010@gmail.com>
2023-12-06 20:49:55 +0300

    reload bugs

--- src/components/OurProjects.vue
+++ src/components/OurProjects.vue
@@ -5,31 +5,36 @@
     <div class="d-flex flex-wrap align-center justify-center">
       <v-card class="card ma-4 pa-4 d-flex flex-column">
         <img src="../assets/img/product_1.png" alt="">
-        <v-card-title class="text-h5 mx-0 px-0 mt-2"> Грузовые лифты </v-card-title>
+        <div class="hidden-sm-and-down"><p class="text-h5 mx-0 px-0 mt-2"> Грузовые лифты </p></div>
+        <div class="hidden-md-and-up"><p class="text-body-1 mx-0 px-0 mt-2"> Грузовые лифты </p></div>
         <v-btn class="linear-gradient-btn my-2" to="/about-lift" block>Подробное</v-btn>
       </v-card>
 
       <v-card class="card ma-4 pa-4 d-flex flex-column">
         <img src="../assets/img/product_2.png" alt="">
-        <v-card-title class="text-h5 mx-0 px-0 mt-2"> Больничные лифты </v-card-title>
+        <div class="hidden-sm-and-down"><p class="text-h5 mx-0 px-0 mt-2"> Больничные лифты </p></div>
+        <div class="hidden-md-and-up"><p class="text-body-1 mx-0 px-0 mt-2"> Больничные лифты </p></div>
         <v-btn class="linear-gradient-btn my-2" to="/about-lift" block>Подробное</v-btn>
       </v-card>
 
       <v-card class="card ma-4 pa-4 d-flex flex-column">
         <img src="../assets/img/product_4.png" alt="">
-        <v-card-title class="text-h5 mx-0 px-0 mt-2"> Лифты для инвалидов </v-card-title>
+        <div class="hidden-sm-and-down"><p class="text-h5 mx-0 px-0 mt-2"> Лифты для инвалидов </p></div>
+        <div class="hidden-md-and-up"><p class="text-body-1 mx-0 px-0 mt-2"> Лифты для инвалидов </p></div>
         <v-btn class="linear-gradient-btn my-2" to="/about-lift" block>Подробное</v-btn>
       </v-card>
 
       <v-card class="card ma-4 pa-4 d-flex flex-column">
         <img src="../assets/img/product_5.png" alt="">
-        <v-card-title class="text-h5 mx-0 px-0 mt-2"> Кухонные лифты </v-card-title>
+        <div class="hidden-sm-and-down"><p class="text-h5 mx-0 px-0 mt-2"> Кухонные лифты </p></div>
+        <div class="hidden-md-and-up"><p class="text-body-1 mx-0 px-0 mt-2"> Кухонные лифты </p></div>
         <v-btn class="linear-gradient-btn my-2" to="/about-lift" block>Подробное</v-btn>
       </v-card>
 
       <v-card class="card ma-4 pa-4 d-flex flex-column">
         <img src="../assets/img/product_5.png" alt="">
-        <v-card-title class="text-h5 mx-0 px-0 mt-2"> Коттеджные лифты </v-card-title>
+        <div class="hidden-sm-and-down"><p class="text-h5 mx-0 px-0 mt-2"> Коттеджные лифты </p></div>
+        <div class="hidden-md-and-up"><p class="text-body-1 mx-0 px-0 mt-2"> Коттеджные лифты </p></div>
         <v-btn class="linear-gradient-btn my-2" to="/about-lift" block>Подробное</v-btn>
       </v-card>
     </div>
