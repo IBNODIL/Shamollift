d21a4d667f87b8c6c65e26e6cf9bd60396123d9a
IBNODIL <ibnodil2010@gmail.com>
2023-12-06 21:14:33 +0300

    add new map and information

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -6,13 +6,13 @@
       <v-text-field label="Номер телефона" variant="solo" color="black" prefix="+998"></v-text-field>
       <v-textarea label="Сообщение" auto-grow variant="solo"></v-textarea>
       <v-btn class="my-3 v-btn--size-small linear-gradient-btn text-black">Оставить заявку<v-icon class="mx-1">mdi-arrow-right</v-icon></v-btn>
-      <p class="d-flex my-2 align-center text-body-1"><img height="25" src="../assets/img/call.svg" class="mr-2" alt=""> +998 94 332 00 16</p>
+      <p class="d-flex my-2 align-center text-body-1"><img height="25" src="../assets/img/call.svg" class="mr-2" alt=""> +998 91 214 34 72</p>
       <p class="d-flex my-2 align-center text-body-1"><img height="25" src="../assets/img/mail.svg" class="mr-2" alt=""> Shamollift@gmail.com</p>
-      <p class="d-flex my-2 align-center text-body-1"><img height="25" src="../assets/img/location_on.svg" class="mr-2" alt=""> Ташкент, Мирзо-Улугбекский район, Шахриабадский район</p>
+      <p class="d-flex my-2 align-center text-body-1"><img height="25" src="../assets/img/location_on.svg" class="mr-2" alt=""> Toshkent shahar Yashnabod tumani Olmos mahallasi Yakkatol 3-tor ko’chasi 38/3 uy</p>
     </div>
 
     <div class="map">
-      <iframe class="this-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.734546666197!2d69.32028657584428!3d41.314638071309155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38aef51d459fd8c1%3A0x37226c9cae6b3453!2sParkent%20Bozori!5e0!3m2!1suz!2s!4v1701708848842!5m2!1suz!2s" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d5996.9787180366175!2d69.365!3d41.276453!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDE2JzM1LjIiTiA2OcKwMjEnNTQuMCJF!5e0!3m2!1suz!2s!4v1701886347552!5m2!1suz!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   </div>
 </template>
